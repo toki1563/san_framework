2fba347003d65fe7a3819a991845639576917b27
toki1563 <sanda1563@icloud.com>
2024-10-22 19:16:14 +0900

    10/22
    DirectInput記入中
    ～初期化

--- san_freamework/framework/directX/san_DirectInput.cpp
+++ san_freamework/framework/directX/san_DirectInput.cpp
@@ -1,5 +1,126 @@
 // DirectInput管理
-
 #include "../../framework.h"
 #include "../san_environment.h"
 
+LPDIRECTINPUT8			sanDirectInput::pDI = NULL;				 // DirectInputオブジェクト
+LPDIRECTINPUTDEVICE8	sanDirectInput::pDIKeyboard = NULL;		 // Keyboard
+LPDIRECTINPUTDEVICE8	sanDirectInput::pDIMouse = NULL;		 // mouse
+LPDIRECTINPUTDEVICE8	sanDirectInput::pDIDev[sanJOYSTICK_MAX]; // joystick
+
+DIMOUSESTATE2	sanDirectInput::diMoue;  // マウスの状態
+int				sanDirectInput::mouse_x; // マウスの座標X
+int				sanDirectInput::mouse_y; // マウスの座標Y
+BYTE			sanDirectInput::diKeyboard[256];  // キーボード用
+BYTE			sanDirectInput::oldKeyboard[256]; // 1フレーム前のキーボードの状態
+DIJOYSTATE		sanDirectInput::diJoystick[sanJOYSTICK_MAX];
+DIJOYSTATE		sanDirectInput::oldJoystick[sanJOYSTICK_MAX];
+
+int				sanDirectInput::joystickButtonNum = sizeof(diJoystick[0].rgbButtons) / sizeof(BYTE);
+int				sanDirectInput::joystickAxisRange = 1000; // スティックの軸の範囲
+float			sanDirectInput::joystickAxisRangeF = 1.0f / (float)joystickAxisRange;
+
+HRESULT sanDirectInput::initialize(void *hinst)
+{
+	HRESULT hr;
+
+	// IDirectInput8インターフェースの取得
+	hr = DirectInput8Create((HINSTANCE)hinst, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&pDI, NULL);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	// ジョイスティック情報の初期化
+	for (int i = 0; i < sanJOYSTICK_MAX; i++)
+	{
+		pDIDev[i] = NULL;
+		ZeroMemory(&diJoystick[i], sizeof(DIJOYSTATE));
+		ZeroMemory(&oldJoystick[i], sizeof(DIJOYSTATE));
+	}
+
+	// IDirectInputDevice8インターフェースの取得
+	// マウス
+	hr = pDI->CreateDevice(GUID_SysMouse, &pDIMouse, NULL);
+	if (FAILED(hr))
+	{
+		terminate();
+		return E_FAIL;
+	}
+	hr = pDIMouse->SetDataFormat(&c_dfDIMouse2); // データフォーマットの設定
+	if (FAILED(hr))
+	{
+		terminate();
+		return E_FAIL;
+	}
+
+	// キーボード
+	hr = pDI->CreateDevice(GUID_SysKeyboard, &pDIKeyboard, NULL);
+	if (FAILED(hr))
+	{
+		terminate();
+		return E_FAIL;
+	}
+	hr = pDIKeyboard->SetDataFormat(&c_dfDIKeyboard);
+	if (FAILED(hr))
+	{
+		terminate();
+		return E_FAIL;
+	}
+
+	// ジョイスティック
+	// デバイスの列挙
+	hr = pDI->EnumDevices(DI8DEVCLASS_GAMECTRL, EnumJoystickCallback, NULL, DIEDFL_ATTACHEDONLY);
+	if (FAILED(hr))
+	{
+		terminate();
+		return E_FAIL;
+	}
+	// データ形式の設定
+	for (int i = 0; i < sanJOYSTICK_MAX; i++)
+	{
+		if (pDIDev[i] == NULL) continue;
+
+		hr = pDIDev[i]->SetDataFormat(&c_dfDIJoystick);
+		if (FAILED(hr))
+		{
+			terminate();
+			return E_FAIL;
+		}
+		// フォアグラウンド&非排他モードの設定
+		// hr = pDIDev[2]->SetCooperativeLevel(hwndApp,DISCL_NONEEXCLUSIVE | DISCL_FOREGROUND);
+		// if(FAILED(hr))
+		// {
+		// 	  return E_FAIL;
+		// }
+		// コールバック関数を使って各軸のモードの設定
+		hr = pDIDev[i]->EnumObjects(EnumAxisCallback, &i, DIDFT_AXIS);
+		if (FAILED(hr))
+		{
+			return E_FAIL;
+		}
+		// 軸モードを絶対値モードに設定
+		DIPROPDWORD diprop;
+		diprop.diph.dwSize = sizeof(diprop);
+		diprop.diph.dwHeaderSize = sizeof(diprop.diph);
+		diprop.diph.dwObj = 0;
+		diprop.diph.dwHow = DIPH_DEVICE;
+		diprop.dwData = DIPROPAXISMODE_ABS;
+		hr = pDIDev[i]->SetProperty(DIPROP_AXISMODE, &diprop.diph);
+		if (FAILED(hr))
+		{
+			return E_FAIL;
+		}
+		// バッファリングデータを取得するためバッファサイズを設定
+		diprop.dwData = 100; // DIDEVICE_BUFFERSIZE
+		hr = pDIDev[i]->SetProperty(DIPROP_BUFFERSIZE, &diprop.diph);
+		if (FAILED(hr))
+		{
+			return E_FAIL;
+		}
+		// 入力制御開始
+		pDIDev[i]->Acquire();
+	}
+
+	if (FAILED(hr)) return hr;
+	return S_OK;
+}
--- san_freamework/framework/directX/san_DirectInput.h
+++ san_freamework/framework/directX/san_DirectInput.h
@@ -11,4 +11,37 @@ private:
 	static LPDIRECTINPUTDEVICE8	pDIMouse;				  // デバイス:mouse
 	static LPDIRECTINPUTDEVICE8	pDIDev[sanJOYSTICK_MAX];  // デバイス:joystick
 
+	static DIMOUSESTATE2 diMoue;		   // マウス用
+	static int           mouse_x;		   // マウス簿座標
+	static int           mouse_y;		   // マウスの座標
+	static BYTE			 diKeyboard[256];  // キーボード用
+	static BYTE          oldKeyboard[256]; // 1フレーム前のキーボードの状態
+	static DIJOYSTATE    diJoystick[sanJOYSTICK_MAX];
+	static DIJOYSTATE    oldJoystick[sanJOYSTICK_MAX];
+
+	static int           joystickButtonNum;
+	static int           joystickAxisRange;
+	static float         joystickAxisRangeF;
+
+	static BOOL CALLBACK EnumJoystickCallback(const DIDEVICEINSTANCE* pdidInstance, VOID* pContext);
+	static BOOL CALLBACK EnumAxisCallback(LPCDIDEVICEOBJECTINSTANCE lpddoi, LPVOID pvRef);
+
+public :
+	static HRESULT initialize(void* hinst);
+	static void terminate();
+
+	static void setMousePosition(int x, int y);
+	static void KeyManager();
+
+	static int getMouseX();
+	static int getMouseY();
+	static DIMOUSESTATE2  *getMouseState();
+	static BYTE			  *getCurrentKeyboardState();
+	static BYTE           *getOldKeyboardState();
+	static DIJOYSTATE     *getCurrentJoystick(int id = 0);
+	static DIJOYSTATE     *getOldJoystick(int id = 0);
+	static bool           useJoystick(int id = 0);
+
+	static int      getJoystickButtomNum();
+	static float	getJoystickAxisRangeF();
 };

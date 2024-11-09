bb8635f3ef3ce276f73951a65b3e51696af66a1a
sanda <sanda.yuto0603@gmail.com>
2024-11-09 10:34:51 +0900

    11/09
    san_DebugDraw完成
    san_collide/san_model/san_object/san_sproteの追加

--- san_freamework/framework/san_joystick.cpp
+++ san_freamework/framework/san_joystick.cpp
@@ -1,3 +1,61 @@
 // ジョイスティック入力(DirectInput)
 #include "../framework.h"
 #include "../framework//san_environment.h"
+
+bool sanJoystick::isValid(int id)
+{
+	return sanDirectInput::useJoystick(id);
+}
+
+bool sanJoystick::on(int button, int id)
+{
+	if (button < 0 || button >= sanDirectInput::getJoystickButtonNum()) return false;
+	DIJOYSTATE* js = sanDirectInput::getCurrentJoystick(id);
+	return js->rgbButtons[button] != 0;
+}
+
+bool sanJoystick::trg(int button, int id)
+{
+	if (button < 0 || button >= sanDirectInput::getJoystickButtonNum()) return false;
+	DIJOYSTATE *jsc = sanDirectInput::getCurrentJoystick(id);
+	DIJOYSTATE *jso = sanDirectInput::getOldJoystick(id);
+	return jsc->rgbButtons[button] != 0 && jso->rgbButtons[button] == 0;
+}
+
+bool sanJoystick::rel(int button, int id)
+{
+	if (button < 0 || button >= sanDirectInput::getJoystickButtonNum()) return false;
+	DIJOYSTATE *jsc = sanDirectInput::getCurrentJoystick(id);
+	DIJOYSTATE *jso = sanDirectInput::getOldJoystick(id);
+	return jsc->rgbButtons[button] == 0 && jso->rgbButtons[button] != 0;
+}
+
+float sanJoystick::leftX(int id)
+{
+	DIJOYSTATE *js = sanDirectInput::getCurrentJoystick(id);
+	return (float)js->lX * sanDirectInput::getJoystickAxisRangeF();
+}
+
+float sanJoystick::leftY(int id)
+{
+	DIJOYSTATE *js = sanDirectInput::getCurrentJoystick(id);
+	return (float)js->lY * sanDirectInput::getJoystickAxisRangeF();
+}
+
+float sanJoystick::rightX(int id)
+{
+	DIJOYSTATE *js = sanDirectInput::getCurrentJoystick(id);
+	return (float)js->lZ * sanDirectInput::getJoystickAxisRangeF();
+}
+
+float sanJoystick::rightY(int id)
+{
+	DIJOYSTATE *js = sanDirectInput::getCurrentJoystick(id);
+	return (float)js->lRz * sanDirectInput::getJoystickAxisRangeF();
+}
+
+int sanJoystick::pov(int id)
+{
+	DIJOYSTATE *js = sanDirectInput::getCurrentJoystick(id);
+	return js->rgdwPOV[0];
+}
--- san_freamework/framework/san_joystick.h
+++ san_freamework/framework/san_joystick.h
@@ -1,2 +1,20 @@
 // ジョイスティック入力(DirectInput)
 #pragma once
+
+class sanJoystick
+{
+public:
+	static bool isValid(int id = 0);	// ジョイスティックが有効かどうか調べる
+
+	static bool on(int button, int id = 0);  // ボタンが押されているか調べる(0~32)
+	static bool trg(int button, int id = 0); // ボタンが押された瞬間か調べる(0~32)
+	static bool rel(int button, int id = 0); // ボタンが離された瞬間か調べる(0~32)
+
+	static float leftX(int id = 0); // 左スティックの左右状態(-1.0~1.0)
+	static float leftY(int id = 0); // 左スティックの上下状態(-1.0~1.0)
+
+	static float rightX(int id = 0); //右スティックの左右状態(-1.0～1.0)
+	static float rightY(int id = 0); //右スティックの上下状態(-1.0～1.0)
+
+	static int pov(int id = 0); // ポイント / ビュー(ハットスイッチ)
+};
